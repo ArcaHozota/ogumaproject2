127bd81f2cc3298dd47f623538de217408b380dd
ArkamaHozota <arkamahozota@gmail.com>
2023-10-11 18:11:34 +0900

    1.86

--- src/main/java/jp/co/toshiba/ppocph/listener/LoginInterceptor.java
+++ src/main/java/jp/co/toshiba/ppocph/listener/LoginInterceptor.java
@@ -35,7 +35,7 @@ public final class LoginInterceptor implements AsyncHandlerInterceptor {
 		final String path = request.getServletPath();
 		// 定义一个数组，存放不需要拦截的路径
 		final String[] excludes = { "/pgcrowd/employee/login", "/pgcrowd/employee/do/login", "/pgcrowd/employee/logout",
-				"/pgcrowd/employee/do/logout" };
+				"/pgcrowd/to/index" };
 		// 遍历数组，判断当前路径是否在其中
 		for (final String exclude : excludes) {
 			// 如果匹配，返回 true，表示不拦截
