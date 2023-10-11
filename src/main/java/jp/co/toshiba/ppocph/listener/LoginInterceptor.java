6ffe840028049a29e15166f06fa5795db2634743
ArkamaHozota <arkamahozota@gmail.com>
2023-10-12 08:50:28 +0900

    1.88

--- src/main/java/jp/co/toshiba/ppocph/listener/LoginInterceptor.java
+++ src/main/java/jp/co/toshiba/ppocph/listener/LoginInterceptor.java
@@ -15,7 +15,7 @@ import jp.co.toshiba.ppocph.entity.Employee;
 import jp.co.toshiba.ppocph.exception.LoginFailedException;
 
 /**
- * アノテーションに基づく例外ハンドラークラス
+ * ログインインターセプター
  *
  * @author ArkamaHozota
  * @since 1.17
