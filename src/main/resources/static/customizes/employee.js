edb4fb5be3887c5d715ee18c09c4b67e0020d822
ArkamaHozota <arkamahozota@gmail.com>
2023-10-13 18:08:27 +0900

    2.54

---
+++ src/main/resources/static/customizes/employee.js
@@ -0,0 +1,37 @@
+$(function() {
+	$(".list-group-item").on('click', function() {
+		if ($(this).find("ul")) {
+			$(this).toggleClass("tree-closed");
+			if ($(this).hasClass("tree-closed")) {
+				$("ul", this).hide("fast");
+			} else {
+				$("ul", this).show("fast");
+			}
+		}
+	});
+	$("#logout-link").on('click', function(e) {
+		e.preventDefault();
+		$("#logout-form").submit();
+	});
+	$("#search-button").on('click', function(e) {
+		let searchInput = $("#search-input").val();
+	});
+	$("#to-mainmenu").on('click', function(e) {
+		e.preventDefault();
+		let username = $("#userinfo").text();
+		$.ajax({
+			url: '/pgcrowd/employee/to/mainmenu',
+			data: 'username=' + username,
+			type: 'GET'
+		});
+	});
+	$("#userinfo2").on('click', function(e) {
+		e.preventDefault();
+		let username = $("#userinfo").text();
+		$.ajax({
+			url: '/pgcrowd/employee/to/pages',
+			data: 'username=' + username,
+			type: 'GET'
+		});
+	});
+});
