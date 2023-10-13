d2bb8a8e6beb6515079f39ad4c0a9aaf85142a4a
ArkamaHozota <arkamahozota@gmail.com>
2023-10-13 17:58:30 +0900

    2.51

--- src/main/java/jp/co/toshiba/ppocph/controller/EmployeeController.java
+++ src/main/java/jp/co/toshiba/ppocph/controller/EmployeeController.java
@@ -1,5 +1,7 @@
 package jp.co.toshiba.ppocph.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -7,11 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import jp.co.toshiba.ppocph.common.PgCrowdConstants;
 import jp.co.toshiba.ppocph.entity.Employee;
 import jp.co.toshiba.ppocph.service.IEmployeeService;
+import jp.co.toshiba.ppocph.utils.ResultDto;
+import jp.co.toshiba.ppocph.utils.StringUtils;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
@@ -64,6 +69,22 @@ public final class EmployeeController {
 	}
 
 	/**
+	 * キーワードによってページング検索
+	 *
+	 * @param pageNum ページ数
+	 * @param keyword キーワード
+	 * @return ResultDto<List<Employee>>
+	 */
+	@GetMapping("/pages")
+	@ResponseBody
+	public ResultDto<List<Employee>> pagination(
+			@RequestParam(name = "page-num", defaultValue = "1") final Integer pageNum,
+			@RequestParam(name = "keyword", defaultValue = StringUtils.EMPTY_STRING) final String keyword) {
+		final List<Employee> employees = this.iEmployeeService.getEmployeesByKeyword(pageNum, keyword);
+		return ResultDto.successWithData(employees);
+	}
+
+	/**
 	 * メインメニューへの移動
 	 *
 	 * @param username ユーザ名称
@@ -74,6 +95,23 @@ public final class EmployeeController {
 		final Employee employee = this.iEmployeeService.getEmployeeByUsername(username);
 		final ModelAndView modelAndView = new ModelAndView("admin-main");
 		modelAndView.addObject(PgCrowdConstants.ATTRNAME_LOGIN_ADMIN, employee);
+		return modelAndView;
+	}
+
+	/**
+	 * 社員情報初期表示
+	 *
+	 * @param pageNum ページ数
+	 * @param keyword キーワード
+	 * @return ModelAndView
+	 */
+	@GetMapping("/to/pages")
+	public ModelAndView toPages(@RequestParam("username") final String username) {
+		final Employee employee = this.iEmployeeService.getEmployeeByUsername(username);
+		final List<Employee> employees = this.iEmployeeService.getEmployeesByKeyword(1, StringUtils.EMPTY_STRING);
+		final ModelAndView modelAndView = new ModelAndView("admin-pages");
+		modelAndView.addObject(PgCrowdConstants.ATTRNAME_LOGIN_ADMIN, employee);
+		modelAndView.addObject(PgCrowdConstants.ATTRNAME_PAGE_INFO, employees);
 		return modelAndView;
 	}
 }
