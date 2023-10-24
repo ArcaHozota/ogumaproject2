7f0c9f045b38cee3bbb5a827687501b0017fde27
ArkamaHozota <arkamahozota@gmail.com>
2023-10-24 18:21:24 +0900

    3.20

--- src/main/resources/static/customizes/employee-pages.js
+++ src/main/resources/static/customizes/employee-pages.js
@@ -1,6 +1,7 @@
 let pageNum = 1;
 let totalRecords, totalPages, keyword;
 $(document).ready(function() {
+	$("#adminKanri").removeClass("collapsed");
 	toSelectedPg(pageNum, keyword);
 });
 $("#searchBtn2").on('click', function() {
