5b6e5a977bf8a2d2d16ce5172d7ff19c5c853069
ArkamaHozota <arkamahozota@gmail.com>
2023-10-24 18:10:13 +0900

    3.16

--- src/main/resources/static/customizes/employee.js
+++ src/main/resources/static/customizes/employee.js
@@ -1,8 +1,8 @@
 $(function() {
 	$(".list-group-item").on('click', function() {
 		if ($(this).find("ul")) {
-			$(this).toggleClass("tree-closed");
-			if ($(this).hasClass("tree-closed")) {
+			$(this).toggleClass("collapsed");
+			if ($(this).hasClass("collapsed")) {
 				$("ul", this).hide("fast");
 			} else {
 				$("ul", this).show("fast");
