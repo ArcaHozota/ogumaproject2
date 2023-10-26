b817cfb8522baa26b5b5c8b75afbc42ba5165184
ArkamaHozota <arkamahozota@gmail.com>
2023-10-26 15:56:41 +0900

    2.52

--- src/main/resources/static/customizes/employee-pages.js
+++ src/main/resources/static/customizes/employee-pages.js
@@ -160,6 +160,55 @@ $("#saveInfoBtn").on('click', function() {
 	$("#inputForm").find(".form-text").text("");
 	if ($(this).attr("ajax-va") === "error") {
 		return false;
+	} else if (inputLoginAccount === "" || inputUsername === "" || inputPassword === "" || inputEmail === "") {
+		if (inputLoginAccount === "" && inputUsername === "" && inputPassword === "" && inputEmail === "") {
+			showValidationMsg("#loginAccountInput", "error", "ログインアカウントを空になってはいけません。");
+			showValidationMsg("#usernameInput", "error", "ユーザ名称を空になってはいけません。");
+			showValidationMsg("#passwordInput", "error", "パスワードを空になってはいけません。");
+			showValidationMsg("#emailInput", "error", "メールアドレスを空になってはいけません。");
+		} else if (inputLoginAccount === "" && inputUsername === "" && inputPassword === "") {
+			showValidationMsg("#loginAccountInput", "error", "ログインアカウントを空になってはいけません。");
+			showValidationMsg("#usernameInput", "error", "ユーザ名称を空になってはいけません。");
+			showValidationMsg("#passwordInput", "error", "パスワードを空になってはいけません。");
+		} else if (inputLoginAccount === "" && inputUsername === "" && inputEmail === "") {
+			showValidationMsg("#loginAccountInput", "error", "ログインアカウントを空になってはいけません。");
+			showValidationMsg("#usernameInput", "error", "ユーザ名称を空になってはいけません。");
+			showValidationMsg("#emailInput", "error", "メールアドレスを空になってはいけません。");
+		} else if (inputLoginAccount === "" && inputPassword === "" && inputEmail === "") {
+			showValidationMsg("#loginAccountInput", "error", "ログインアカウントを空になってはいけません。");
+			showValidationMsg("#passwordInput", "error", "パスワードを空になってはいけません。");
+			showValidationMsg("#emailInput", "error", "メールアドレスを空になってはいけません。");
+		} else if (inputUsername === "" && inputPassword === "" && inputEmail === "") {
+			showValidationMsg("#usernameInput", "error", "ユーザ名称を空になってはいけません。");
+			showValidationMsg("#passwordInput", "error", "パスワードを空になってはいけません。");
+			showValidationMsg("#emailInput", "error", "メールアドレスを空になってはいけません。");
+		} else if (inputPassword === "" && inputEmail === "") {
+			showValidationMsg("#passwordInput", "error", "パスワードを空になってはいけません。");
+			showValidationMsg("#emailInput", "error", "メールアドレスを空になってはいけません。");
+		} else if (inputLoginAccount === "" && inputUsername === "") {
+			showValidationMsg("#loginAccountInput", "error", "ログインアカウントを空になってはいけません。");
+			showValidationMsg("#usernameInput", "error", "ユーザ名称を空になってはいけません。");
+		} else if (inputLoginAccount === "" && inputEmail === "") {
+			showValidationMsg("#loginAccountInput", "error", "ログインアカウントを空になってはいけません。");
+			showValidationMsg("#emailInput", "error", "メールアドレスを空になってはいけません。");
+		} else if (inputUsername === "" && inputPassword === "") {
+			showValidationMsg("#usernameInput", "error", "ユーザ名称を空になってはいけません。");
+			showValidationMsg("#passwordInput", "error", "パスワードを空になってはいけません。");
+		} else if (inputUsername === "" && inputEmail === "") {
+			showValidationMsg("#usernameInput", "error", "ユーザ名称を空になってはいけません。");
+			showValidationMsg("#emailInput", "error", "メールアドレスを空になってはいけません。");
+		} else if (inputLoginAccount === "" && inputPassword === "") {
+			showValidationMsg("#loginAccountInput", "error", "ログインアカウントを空になってはいけません。");
+			showValidationMsg("#passwordInput", "error", "パスワードを空になってはいけません。");
+		} else if (inputLoginAccount === "") {
+			showValidationMsg("#loginAccountInput", "error", "ログインアカウントを空になってはいけません。");
+		} else if (inputUsername === "") {
+			showValidationMsg("#usernameInput", "error", "ユーザ名称を空になってはいけません。");
+		} else if (inputPassword === "") {
+			showValidationMsg("#passwordInput", "error", "パスワードを空になってはいけません。");
+		} else {
+			showValidationMsg("#emailInput", "error", "メールアドレスを空になってはいけません。");
+		}
 	} else {
 		/*$.ajax({
 			url: '/jpasample/city',
