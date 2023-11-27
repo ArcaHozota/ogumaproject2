c7fee788ba1ec49e6b5e872d00115c801f490952
ArkamaHozota <arkamahozota@gmail.com>
2023-11-27 17:10:56 +0900

    4.44

---
+++ src/main/java/jp/co/toshiba/ppocph/controller/RoleController.java
@@ -0,0 +1,68 @@
+package jp.co.toshiba.ppocph.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import jp.co.toshiba.ppocph.common.PgCrowdConstants;
+import jp.co.toshiba.ppocph.entity.Employee;
+import jp.co.toshiba.ppocph.entity.Role;
+import jp.co.toshiba.ppocph.service.IEmployeeService;
+import jp.co.toshiba.ppocph.utils.Pagination;
+import jp.co.toshiba.ppocph.utils.ResultDto;
+import jp.co.toshiba.ppocph.utils.StringUtils;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 役割コントローラ
+ *
+ * @author ArkamaHozota
+ * @since 4.44
+ */
+@Controller
+@RequestMapping("/pgcrowd/role")
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public final class RoleController {
+
+	/**
+	 * 社員サービスインターフェス
+	 */
+	private final IEmployeeService iEmployeeService;
+
+	/**
+	 * 役割情報初期表示
+	 *
+	 * @param userId  ユーザID
+	 * @param pageNum ページナンバー
+	 * @return ModelAndView
+	 */
+	@GetMapping("/to/pages")
+	public ModelAndView initialPages(@RequestParam("userId") final Integer userId,
+			@RequestParam(name = "pageNum") final Integer pageNum) {
+		final Employee employee = this.iEmployeeService.getEmployeeById(userId);
+		final ModelAndView modelAndView = new ModelAndView("role-pages");
+		modelAndView.addObject(PgCrowdConstants.ATTRNAME_LOGIN_ADMIN, employee);
+		modelAndView.addObject(PgCrowdConstants.ATTRNAME_PAGE_NUMBER, pageNum);
+		return modelAndView;
+	}
+
+	/**
+	 * キーワードによってページング検索
+	 *
+	 * @param pageNum ページ数
+	 * @param keyword キーワード
+	 * @return ResultDto<List<Employee>>
+	 */
+	@GetMapping("/pagination")
+	@ResponseBody
+	public ResultDto<Pagination<Role>> pagination(
+			@RequestParam(name = "pageNum", defaultValue = "1") final Integer pageNum,
+			@RequestParam(name = "keyword", defaultValue = StringUtils.EMPTY_STRING) final String keyword) {
+		final Pagination<Role> roles = this.iRoleService.getRolesByKeyword(pageNum, keyword);
+		return ResultDto.successWithData(roles);
+	}
+}
