738b76789d4465920af4b75f4ec9df4d7496a2f7
ArkamaHozota <arkamahozota@gmail.com>
2023-12-11 12:01:53 +0900

    5.31

--- src/main/java/jp/co/toshiba/ppocph/exception/PgCrowdException.java
+++ src/main/java/jp/co/toshiba/ppocph/exception/PgCrowdException.java
@@ -1,5 +1,7 @@
 package jp.co.toshiba.ppocph.exception;
 
+import jp.co.toshiba.ppocph.utils.StringUtils;
+
 /**
  * プロジェクト業務ロジック例外
  *
@@ -10,11 +12,23 @@ public class PgCrowdException extends RuntimeException {
 
 	private static final long serialVersionUID = 8469408957890840211L;
 
+	/**
+	 * メッセージ
+	 */
+	private final String message;
+
 	public PgCrowdException() {
 		super();
+		this.message = StringUtils.EMPTY_STRING;
 	}
 
 	public PgCrowdException(final String message) {
 		super(message);
+		this.message = message;
+	}
+
+	@Override
+	public String getMessage() {
+		return this.message;
 	}
 }
