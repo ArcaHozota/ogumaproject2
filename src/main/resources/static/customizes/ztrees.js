5a82ef30a75949504252e3c56dc9c26befbab981
ArkamaHozota <arkamahozota@gmail.com>
2023-12-11 18:04:35 +0900

    5.48

--- src/main/resources/static/customizes/ztrees.js
+++ src/main/resources/static/customizes/ztrees.js
@@ -47,13 +47,13 @@ $(document).ready(function() {
 						]
 					}
 				];
-				$('#tree').treeview({
+				$("#tree").treeview({
 					data: treeData, // 设置树形视图的数据源
 					levels: 2, // 设置树形视图的默认展开层级
 					color: "#000", // 设置树形视图的字体颜色
 					backColor: "#fff", // 设置树形视图的背景颜色
 					onhoverColor: "orange", // 设置树形视图的鼠标悬停颜色
-					borderColor: "red", // 设置树形视图的边框颜色
+					borderColor: "green", // 设置树形视图的边框颜色
 					onNodeSelected: function(event, data) { // 设置树形视图的节点选中事件
 						alert("你选择了" + data.text + "节点"); // 弹出提示框，显示选中的节点的文本
 					}
