f9f5a4bef3c53c011ee6602524ba9b96e7a657a3
ArkamaHozota <arkamahozota@gmail.com>
2023-12-11 17:36:21 +0900

    5.46

--- src/main/resources/static/customizes/ztrees.js
+++ src/main/resources/static/customizes/ztrees.js
@@ -7,56 +7,58 @@ $(document).ready(function() {
 		type: 'GET',
 		dataType: 'json',
 		success: function(result) {
-			let authlist = result.data;
-			treeData = [
-				{
-					text: authlist.eq(0).name,
-					icon: "glyphicon glyphicon-folder-open",
-					nodes: [
-						{
-							text: authlist.eq(1).name,
-							icon: "glyphicon glyphicon-file"
-						},
-						{
-							text: authlist.eq(2).name,
-							icon: "glyphicon glyphicon-file"
-						},
-						{
-							text: authlist.eq(3).name,
-							icon: "glyphicon glyphicon-file"
-						}
-					]
-				},
-				{
-					text: authlist.eq(4).name,
-					icon: "glyphicon glyphicon-folder-open",
-					nodes: [
-						{
-							text: authlist.eq(5).name,
-							icon: "glyphicon glyphicon-file"
-						},
-						{
-							text: authlist.eq(6).name,
-							icon: "glyphicon glyphicon-file"
-						},
-						{
-							text: authlist.eq(7).name,
-							icon: "glyphicon glyphicon-file"
-						}
-					]
-				}
-			];
-			$('#tree').treeview({
-				data: treeData, // 设置树形视图的数据源
-				levels: 2, // 设置树形视图的默认展开层级
-				color: "#000", // 设置树形视图的字体颜色
-				backColor: "#fff", // 设置树形视图的背景颜色
-				onhoverColor: "orange", // 设置树形视图的鼠标悬停颜色
-				borderColor: "red", // 设置树形视图的边框颜色
-				onNodeSelected: function(event, data) { // 设置树形视图的节点选中事件
-					alert("你选择了" + data.text + "节点"); // 弹出提示框，显示选中的节点的文本
-				}
-			});
+			if (result.status === 'SUCCESS') {
+				let authlist = result.data;
+				treeData = [
+					{
+						text: authlist.eq(0).name,
+						icon: "glyphicon glyphicon-folder-open",
+						nodes: [
+							{
+								text: authlist.eq(1).name,
+								icon: "glyphicon glyphicon-file"
+							},
+							{
+								text: authlist.eq(2).name,
+								icon: "glyphicon glyphicon-file"
+							},
+							{
+								text: authlist.eq(3).name,
+								icon: "glyphicon glyphicon-file"
+							}
+						]
+					},
+					{
+						text: authlist.eq(4).name,
+						icon: "glyphicon glyphicon-folder-open",
+						nodes: [
+							{
+								text: authlist.eq(5).name,
+								icon: "glyphicon glyphicon-file"
+							},
+							{
+								text: authlist.eq(6).name,
+								icon: "glyphicon glyphicon-file"
+							},
+							{
+								text: authlist.eq(7).name,
+								icon: "glyphicon glyphicon-file"
+							}
+						]
+					}
+				];
+				$('#tree').treeview({
+					data: treeData, // 设置树形视图的数据源
+					levels: 2, // 设置树形视图的默认展开层级
+					color: "#000", // 设置树形视图的字体颜色
+					backColor: "#fff", // 设置树形视图的背景颜色
+					onhoverColor: "orange", // 设置树形视图的鼠标悬停颜色
+					borderColor: "red", // 设置树形视图的边框颜色
+					onNodeSelected: function(event, data) { // 设置树形视图的节点选中事件
+						alert("你选择了" + data.text + "节点"); // 弹出提示框，显示选中的节点的文本
+					}
+				});
+			}
 		}
 	});
 });
