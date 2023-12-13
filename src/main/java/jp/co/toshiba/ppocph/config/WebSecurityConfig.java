e995ad35ee99018921226584b07f7957a3b0660a
ArkamaHozota <arkamahozota@gmail.com>
2023-12-13 23:57:07 +0900

    6.10

--- src/main/java/jp/co/toshiba/ppocph/config/WebSecurityConfig.java
+++ src/main/java/jp/co/toshiba/ppocph/config/WebSecurityConfig.java
@@ -1,15 +1,22 @@
 package jp.co.toshiba.ppocph.config;
 
+import javax.annotation.Resource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import jp.co.toshiba.ppocph.common.PgCrowdConstants;
 import jp.co.toshiba.ppocph.exception.PgCrowdException;
+import jp.co.toshiba.ppocph.listener.PgCrowdUserDetailsService;
 
 /**
  * SpringSecurity配置クラス
@@ -20,6 +27,22 @@ import jp.co.toshiba.ppocph.exception.PgCrowdException;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+	@Resource
+	private PgCrowdUserDetailsService pgCrowdUserDetailsService;
+
+	@Bean
+	protected AuthenticationManager authenticationManager(final AuthenticationManagerBuilder auth) throws Exception {
+		return auth.authenticationProvider(this.daoAuthenticationProvider()).build();
+	}
+
+	@Bean
+	protected DaoAuthenticationProvider daoAuthenticationProvider() {
+		final DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(this.pgCrowdUserDetailsService);
+		provider.setPasswordEncoder(new BCryptPasswordEncoder());
+		return provider;
+	}
 
 	@Bean
 	protected SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
@@ -42,5 +65,4 @@ public class WebSecurityConfig {
 		}).rememberMe(Customizer.withDefaults());
 		return http.build();
 	}
-
 }
