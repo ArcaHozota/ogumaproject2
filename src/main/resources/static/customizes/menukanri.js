e5e3d3ad935e35ae748e7c98ab6e645a6008d88d
arkamahozota <arkamahozota@localhost>
2023-12-15 16:56:50 +0900

    6.46

--- src/main/resources/static/customizes/menukanri.js
+++ src/main/resources/static/customizes/menukanri.js
@@ -30,17 +30,20 @@ $(document).ready(function() {
 			]
 		}
 	];
-	$('#treeView').bstreeview({
-		data: treeData,
-		expandIcon: 'fa fa-angle-down fa-fw',
-		collapseIcon: 'fa fa-angle-right fa-fw',
-		indent: 2,
-		parentsMarginLeft: '1.25rem',
-		openNodeLinkOnNewTab: true
+	$('#treeView').treeview({
+		data: treeData, // 设置树形视图的数据源
+		levels: 2, // 设置树形视图的默认展开层级
+		color: "#000", // 设置树形视图的字体颜色
+		backColor: "#fff", // 设置树形视图的背景颜色
+		onhoverColor: "orange", // 设置树形视图的鼠标悬停颜色
+		borderColor: "red", // 设置树形视图的边框颜色
+		onNodeSelected: function(event, data) { // 设置树形视图的节点选中事件
+			alert("你选择了" + data.text + "节点"); // 弹出提示框，显示选中的节点的文本
+		}
 	});
 });
-$("#treeView").on('click', '.list-group-item', function() {
-	let titleName = $(this).text();
+$("#treeView").on('nodeSelected', function(event, data) {
+	let titleName = data.text;
 	switch (titleName) {
 		case "社員情報追加":
 			window.location.replace("/pgcrowd/employee/to/addition");
