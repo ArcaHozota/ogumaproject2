2d49bfcf16adb149f59d56c5452aa771d0b9236f
arkamahozota <arkamahozota@localhost>
2023-12-15 15:42:58 +0900

    6.42

--- src/main/resources/static/customizes/menukanri.js
+++ src/main/resources/static/customizes/menukanri.js
@@ -1,4 +1,3 @@
-let treeData;
 let pageNum = $("#pageNumber").val();
 $(document).ready(function() {
 	$("#adminKanri").removeClass('collapsed');
@@ -6,30 +5,30 @@ $(document).ready(function() {
 	let treeData = [
 		{
 			text: "社員管理",
-			icon: "bi bi-1-circle-fill",
+			icon: "bi bi-person-circle",
 			nodes: [
 				{
 					text: "社員情報追加",
-					icon: "bi bi-2-circle-fill"
+					icon: "bi bi-person-fill-add"
 				},
 				{
 					text: "社員情報一覧",
-					icon: "bi bi-3-circle-fill"
+					icon: "bi bi-person-check"
 				}
 			]
 		},
 		{
 			text: "役割管理",
-			icon: "bi bi-5-circle-fill",
+			icon: "bi bi-person-badge-fill",
 			nodes: [
 				{
 					text: "役割情報一覧",
-					icon: "bi bi-6-circle-fill"
+					icon: "bi bi-person-vcard"
 				}
 			]
 		}
 	];
-	$('#tree').bstreeview({
+	$('#treeView').bstreeview({
 		data: treeData,
 		expandIcon: 'fa fa-angle-down fa-fw',
 		collapseIcon: 'fa fa-angle-right fa-fw',
