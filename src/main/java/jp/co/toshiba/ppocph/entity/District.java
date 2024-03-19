2e6b8a77913f7691c2ff224e7c531c34f7fe62bd
ArkamaHozota <arkamahozota@hotmail.com>
2024-03-19 14:05:35 +0900

    7.80

---
+++ src/main/java/jp/co/toshiba/ppocph/entity/District.java
@@ -0,0 +1,66 @@
+package jp.co.toshiba.ppocph.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 地域エンティティ
+ *
+ * @author ArkamaHozota
+ * @since 7.80
+ */
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Table(name = "districts")
+@EqualsAndHashCode(callSuper = false)
+public class District implements Serializable {
+
+	private static final long serialVersionUID = -3956143435944525913L;
+
+	/**
+	 * ID
+	 */
+	@Id
+	private Long id;
+
+	/**
+	 * 名称
+	 */
+	@Column(nullable = false)
+	private String name;
+
+	/**
+	 * 地方名称
+	 */
+	@Column(nullable = false)
+	private String chiho;
+
+	/**
+	 * 都道府県旗
+	 */
+	private String districtFlag;
+
+	/**
+	 * 州都ID
+	 */
+	@Column(nullable = false)
+	private Long shutoId;
+
+	/**
+	 * 論理削除フラグ
+	 */
+	@Column(nullable = false)
+	private String deleteFlg;
+}
