66e1a63dbf3bcc47236716e02a83cc7d21f59081
ArkamaHozota <arkamahozota@hotmail.com>
2024-03-19 14:07:34 +0900

    7.81

---
+++ src/main/java/jp/co/toshiba/ppocph/service/impl/DistrictServiceImpl.java
@@ -0,0 +1,18 @@
+package jp.co.toshiba.ppocph.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import jp.co.toshiba.ppocph.service.IDistrictService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 地域サービス実装クラス
+ *
+ * @author ArkamaHozota
+ * @since 7.81
+ */
+@Service
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class DistrictServiceImpl implements IDistrictService {
+}
