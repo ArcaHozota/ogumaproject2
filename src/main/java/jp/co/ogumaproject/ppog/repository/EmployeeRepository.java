28ce58bc612a2b09e1f22e44e2a43bf3c2380cf6
ArkamaHozota <arkamahozota@hotmail.com>
2024-05-17 17:41:55 +0900

    9.70

--- src/main/java/jp/co/ogumaproject/ppog/repository/EmployeeRepository.java
+++ src/main/java/jp/co/ogumaproject/ppog/repository/EmployeeRepository.java
@@ -26,7 +26,7 @@ public class EmployeeRepository implements CommonRepository<Employee> {
 	@Override
 	public List<Employee> getListByForeignKey(final Long foreignKey) {
 		return this.jdbcClient.sql(
-				"SELECT PEV.* FROM PPOG_EMPLOYEE_VIEW PEV INNER JOIN PPOG_EMPLOYEE_ROLE_VIEW PERV ON PERV.EMPLOYEE_ID = REV.ID　"
+				"SELECT PEV.* FROM PPOG_EMPLOYEE_VIEW PEV INNER JOIN PPOG_EMPLOYEE_ROLE_VIEW PERV ON PERV.EMPLOYEE_ID = PEV.ID　"
 						+ "WHERE PERV.ROLE_ID = ?")
 				.param(foreignKey).query(Employee.class).list();
 	}
