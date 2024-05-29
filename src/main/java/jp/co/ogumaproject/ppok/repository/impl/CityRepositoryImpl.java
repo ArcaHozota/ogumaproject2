b7893b2dcddba5bbfc6e3605ca0c650e77bfea51
ArkamaHozota <arkamahozota@hotmail.com>
2024-05-29 15:49:48 +0900

    10.15

--- src/main/java/jp/co/ogumaproject/ppok/repository/impl/CityRepositoryImpl.java
+++ src/main/java/jp/co/ogumaproject/ppok/repository/impl/CityRepositoryImpl.java
@@ -1,6 +1,7 @@
 package jp.co.ogumaproject.ppok.repository.impl;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import jakarta.annotation.Resource;
 import jp.co.ogumaproject.ppok.entity.City;
 import jp.co.ogumaproject.ppok.repository.CityRepository;
+import jp.co.ogumaproject.ppok.utils.OgumaProjectUtils;
 
 /**
  * 都市リポジトリ
@@ -74,20 +76,25 @@ public class CityRepositoryImpl implements CityRepository {
 
 	@Override
 	public void removeById(final City aEntity) {
-		// TODO Auto-generated method stub
-
+		final Map<String, Object> paramMap = OgumaProjectUtils.getParamMap(aEntity);
+		this.jdbcClient.sql("UPDATE PPOG_CITIES PC SET PC.DEL_FLG =:delFlg WHERE PC.ID =:id").params(paramMap).update();
 	}
 
 	@Override
 	public void saveById(final City aEntity) {
-		// TODO Auto-generated method stub
-
+		final Map<String, Object> paramMap = OgumaProjectUtils.getParamMap(aEntity);
+		this.jdbcClient.sql(
+				"INSERT INTO PPOG_CITIES PC (PC.ID, PC.NAME, PC.PRONUNCIATION, PC.DISTRICT_ID, PC.CITY_FLAG, PC.POPULATION, PC.DEL_FLG) "
+						+ "VALUES (:id, :name, :pronunciation, :districtId, :cityFlag, :population, :delFlg)")
+				.params(paramMap).update();
 	}
 
 	@Override
 	public void updateById(final City aEntity) {
-		// TODO Auto-generated method stub
-
+		final Map<String, Object> paramMap = OgumaProjectUtils.getParamMap(aEntity);
+		this.jdbcClient.sql(
+				"UPDATE PPOG_CITIES PC SET PC.NAME =:id, PC.PRONUNCIATION =:pronunciation, PC.DISTRICT_ID =:districtId, "
+						+ "PC.CITY_FLAG =:cityFlag, PC.POPULATION =:population WHERE PC.DEL_FLG =:delFlg AND PC.ID =:id")
+				.params(paramMap).update();
 	}
-
 }
