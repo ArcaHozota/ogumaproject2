77c63332ea3f46860d6e40d3b2b2b4d689c1d445
ArkamaHozota <arkamahozota@hotmail.com>
2024-08-19 16:39:40 +0900

    10.44

--- src/main/java/jp/co/ogumaproject/ppok/repository/impl/DistrictRepositoryImpl.java
+++ src/main/java/jp/co/ogumaproject/ppok/repository/impl/DistrictRepositoryImpl.java
@@ -1,15 +1,12 @@
 package jp.co.ogumaproject.ppok.repository.impl;
 
 import java.util.List;
-import java.util.Map;
 
-import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
-import jakarta.annotation.Resource;
+import jakarta.annotation.PostConstruct;
 import jp.co.ogumaproject.ppok.entity.District;
 import jp.co.ogumaproject.ppok.repository.DistrictRepository;
-import jp.co.ogumaproject.ppok.utils.OgumaProjectUtils;
 
 /**
  * 地域リポジトリ
@@ -18,63 +15,62 @@ import jp.co.ogumaproject.ppok.utils.OgumaProjectUtils;
  * @since 9.97
  */
 @Repository
-public class DistrictRepositoryImpl implements DistrictRepository {
-
-	/**
-	 * JDBCクライアント
-	 */
-	@Resource
-	private JdbcClient jdbcClient;
+public class DistrictRepositoryImpl extends CommonRepositoryImpl<District> implements DistrictRepository {
 
 	@Override
 	public Integer countByKeyword(final String keyword) {
-		return this.jdbcClient.sql(
-				"SELECT COUNT(1) FROM PPOG_DISTRICTS_VIEW PDV INNER JOIN PPOG_CHIHOS_VIEW PCHV ON PCHV.ID = PDV.CHIHO_ID "
-						+ "INNER JOIN PPOG_CITIES_VIEW PCV ON PCV.ID = PDV.SHUTO_ID WHERE PDV.NAME LIKE ? OR PCV.NAME LIKE ? "
-						+ "OR PCHV.NAME LIKE ?")
-				.params(keyword, keyword, keyword).query(Integer.class).single();
+		final String sql = "SELECT COUNT(1) FROM PPOG_DISTRICTS_VIEW PDV INNER JOIN PPOG_CHIHOS_VIEW PCHV ON PCHV.ID = PDV.CHIHO_ID "
+				+ "INNER JOIN PPOG_CITIES_VIEW PCV ON PCV.ID = PDV.SHUTO_ID WHERE PDV.NAME LIKE ? OR PCV.NAME LIKE ? "
+				+ "OR PCHV.NAME LIKE ?";
+		return this.commonCountByKeywords(sql, keyword, keyword, keyword).intValue();
 	}
 
 	@Override
 	public Integer countByShutoId(final Long shutoId) {
-		return this.jdbcClient.sql("SELECT COUNT(1) FROM PPOG_DISTRICTS_VIEW PDV WHERE PDV.SHUTO_ID = ?").param(shutoId)
-				.query(Integer.class).single();
+		final String sql = "SELECT COUNT(1) FROM PPOG_DISTRICTS_VIEW PDV WHERE PDV.SHUTO_ID = ?";
+		return this.commonCountByKeywords(sql, shutoId).intValue();
 	}
 
 	@Override
 	public List<District> getList() {
-		return this.jdbcClient.sql("SELECT PDV.* FROM PPOG_DISTRICTS_VIEW PDV ORDER BY PDV.ID ASC")
-				.query(District.class).list();
+		final String sql = "SELECT PDV.* FROM PPOG_DISTRICTS_VIEW PDV ORDER BY PDV.ID ASC";
+		return this.getCommonListByKeywords(sql);
 	}
 
 	@Override
 	public List<District> getListByForeignKey(final Long foreignKey) {
-		return this.jdbcClient.sql(
-				"SELECT PDV.* FROM PPOG_DISTRICTS_VIEW PDV INNER JOIN PPOG_CHIHOS_VIEW PCHV ON PCHV.ID = PDV.CHIHO_ID "
-						+ "WHERE PCHV.ID = ? ORDER BY PDV.ID ASC")
-				.param(foreignKey).query(District.class).list();
+		final String sql = "SELECT PDV.* FROM PPOG_DISTRICTS_VIEW PDV INNER JOIN PPOG_CHIHOS_VIEW PCHV ON PCHV.ID = PDV.CHIHO_ID "
+				+ "WHERE PCHV.ID = ? ORDER BY PDV.ID ASC";
+		return this.getCommonListByForeignKey(sql, foreignKey);
 	}
 
 	@Override
 	public List<District> getListByIds(final List<Long> ids) {
-		return this.jdbcClient.sql("SELECT PDV.* FROM PPOG_DISTRICTS_VIEW PDV WHERE PDV.ID IN (?) ORDER BY PDV.ID ASC")
-				.params(ids).query(District.class).list();
+		final String sql = "SELECT PDV.* FROM PPOG_DISTRICTS_VIEW PDV WHERE PDV.ID IN (?) ORDER BY PDV.ID ASC";
+		return this.getCommonListByIds(sql, ids);
 	}
 
 	@Override
 	public District getOneById(final Long id) {
-		return this.jdbcClient.sql("SELECT PDV.* FROM PPOG_DISTRICTS_VIEW PDV WHERE PDV.ID = ?").param(id)
-				.query(District.class).single();
+		final String sql = "SELECT PDV.* FROM PPOG_DISTRICTS_VIEW PDV WHERE PDV.ID = ?";
+		return this.getCommonOneById(sql, id);
+	}
+
+	/**
+	 * イニシャル
+	 */
+	@PostConstruct
+	private void initial() {
+		this.setEntityClass(District.class);
 	}
 
 	@Override
 	public List<District> pagination(final Integer offset, final Integer pageSize, final String keyword) {
-		return this.jdbcClient
-				.sql("SELECT PDV.* FROM PPOG_DISTRICTS_VIEW PDV INNER JOIN PPOG_CHIHOS_VIEW PCHV "
-						+ "ON PCHV.ID = PDV.CHIHO_ID INNER JOIN PPOG_CITIES_VIEW PCV ON PCV.ID = PDV.SHUTO_ID "
-						+ "WHERE PDV.NAME LIKE ? OR PCV.NAME LIKE ? OR PCHV.NAME LIKE ? ORDER BY PDV.ID ASC "
-						+ "OFFSET ? ROWS FETCH NEXT ? ROWS ONLY")
-				.params(keyword, keyword, keyword, offset, pageSize).query(District.class).list();
+		final String sql = "SELECT PDV.* FROM PPOG_DISTRICTS_VIEW PDV INNER JOIN PPOG_CHIHOS_VIEW PCHV "
+				+ "ON PCHV.ID = PDV.CHIHO_ID INNER JOIN PPOG_CITIES_VIEW PCV ON PCV.ID = PDV.SHUTO_ID "
+				+ "WHERE PDV.NAME LIKE ? OR PCV.NAME LIKE ? OR PCHV.NAME LIKE ? ORDER BY PDV.ID ASC "
+				+ "OFFSET ? ROWS FETCH NEXT ? ROWS ONLY";
+		return this.getCommonListByKeywords(sql, keyword, keyword, keyword, offset, pageSize);
 	}
 
 	@Deprecated
@@ -89,11 +85,9 @@ public class DistrictRepositoryImpl implements DistrictRepository {
 
 	@Override
 	public void updateById(final District aEntity) {
-		final Map<String, Object> paramMap = OgumaProjectUtils.getParamMap(aEntity);
-		this.jdbcClient.sql(
-				"UPDATE PPOG_DISTRICTS PD SET PD.NAME =:name, PD.CHIHO_ID =:chihoId, PD.SHUTO_ID =:shutoId, PD.DISTRICT_FLAG =:districtFlag "
-						+ "WHERE PD.DEL_FLG =:delFlg AND PD.ID =:id")
-				.params(paramMap).update();
+		final String sql = "UPDATE PPOG_DISTRICTS PD SET PD.NAME =:name, PD.CHIHO_ID =:chihoId, PD.SHUTO_ID =:shutoId, PD.DISTRICT_FLAG =:districtFlag "
+				+ "WHERE PD.DEL_FLG =:delFlg AND PD.ID =:id";
+		this.commonModifyById(sql, aEntity);
 	}
 
 }
